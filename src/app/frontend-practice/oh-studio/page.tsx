6ef216d5510c351021218dac235864c052805383
Bryan <blinz117@gmail.com>
2023-08-04 03:44:24 -0500

    Tweak a bunch of styles

--- src/app/frontend-practice/oh-studio/page.tsx
+++ src/app/frontend-practice/oh-studio/page.tsx
@@ -1,8 +1,25 @@
 import Link from "next/link";
+import localFont from "next/font/local";
+
+const font = localFont({
+  src: [
+    {
+      path: "./assets/NeueMontreal-Medium.otf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "./assets/NeueMontreal-Regular.otf",
+      weight: "400",
+      style: "regular",
+    },
+  ],
+  variable: "--font-neue-montreal",
+});
 
 function NavBarItem(props: { text: string; href: string }) {
   return (
-    <Link className="px-6 py-2" href={props.href}>
+    <Link className="px-6 py-2.5" href={props.href}>
       {props.text}
     </Link>
   );
@@ -10,7 +27,7 @@ function NavBarItem(props: { text: string; href: string }) {
 
 function ExpertiseListItem(props: { text: string }) {
   return (
-    <li className="rounded-full bg-gray-200 bg-opacity-50 px-2 py-1">
+    <li className="rounded-full bg-stone-200 bg-opacity-50 px-2.5 py-1.5">
       {props.text}
     </li>
   );
@@ -18,21 +35,23 @@ function ExpertiseListItem(props: { text: string }) {
 
 function ProjectGridItem(props: { text: string }) {
   return (
-    <div className="aspect-video rounded-xl bg-gray-300">{props.text}</div>
+    <div className="aspect-[4/3] rounded-xl bg-gray-300">{props.text}</div>
   );
 }
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <nav className="fixed top-8 flex flex-row gap-2 rounded-full bg-gray-200 bg-opacity-50 p-1 text-sm backdrop-blur">
+    <main
+      className={`flex min-h-screen flex-col items-center ${font.variable} font-sans`}
+    >
+      <nav className="fixed top-8 flex flex-row gap-2 rounded-full bg-stone-200 bg-opacity-50 p-1 text-sm backdrop-blur">
         <NavBarItem href={"/frontend-practice/oh-studio"} text="Home" />
         <NavBarItem href={"/frontend-practice/oh-studio"} text="Profile" />
         <NavBarItem href={"/frontend-practice/oh-studio"} text="Contact" />
       </nav>
 
       <header className="mt-24 flex min-h-[379px] flex-col items-center justify-evenly p-5 md:p-10">
-        <h1 className="max-w-md text-center text-5xl">
+        <h1 className="max-w-xl break-words text-center text-5xl font-medium leading-tight">
           A web and mobile developer working with clients globally
         </h1>
         <ul className="hidden flex-row items-center gap-2.5 text-sm md:inline-flex">
@@ -43,26 +62,40 @@ export default function Home() {
         </ul>
       </header>
 
-      <section className="grid w-full grid-cols-1 gap-4 p-5 md:grid-cols-2 md:p-10">
+      <section className="grid w-full grid-cols-1 gap-x-4 gap-y-10 p-5 md:grid-cols-2 md:p-10">
         <ProjectGridItem text="Portfolio" />
         <ProjectGridItem text="Creative Developer" />
         <ProjectGridItem text="SongSpark" />
       </section>
 
-      <section className="my-8 flex flex-col items-center justify-center p-5 text-2xl md:p-10">
+      <section className="my-8 flex flex-col items-center justify-center p-5 text-4xl font-medium leading-tight md:p-10">
         {"Let's work together."}
-        <a href="#">{"Get in touch."}</a>
+        <a href="#" className="text-neutral-500 hover:text-neutral-400">
+          {"Get in touch."}
+        </a>
       </section>
 
       <footer className="flex w-full flex-col justify-between p-5 text-sm md:flex-row md:p-10">
         <div className="flex flex-col items-center gap-2 md:flex-row">
-          <div className="aspect-square w-6 rounded-full bg-green-500"></div>
+          <div className="aspect-square w-6 rounded-full bg-black"></div>
           <a href="https://oh.studio/">{"Design by Oli Harris"}</a>
         </div>
         <div className="flex flex-row items-center justify-center gap-4">
-          <a href="https://github.com/bryndsey">Github</a>
-          <a href="https://www.linkedin.com/in/bryan-c-lindsey/">LinkedIn</a>
-          <a href="#">Mail</a>
+          <a
+            href="https://github.com/bryndsey"
+            className="hover:text-neutral-500"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bryan-c-lindsey/"
+            className="hover:text-neutral-500"
+          >
+            LinkedIn
+          </a>
+          <a href="#" className="hover:text-neutral-500">
+            Mail
+          </a>
         </div>
       </footer>
     </main>
