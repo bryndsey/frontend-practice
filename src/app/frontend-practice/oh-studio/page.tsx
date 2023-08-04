769cb807f9b0d6a64aad13a8406b56ec001cdd82
Bryan <blinz117@gmail.com>
2023-08-03 20:27:39 -0500

    Set up tailwind prettier plugin

--- src/app/frontend-practice/oh-studio/page.tsx
+++ src/app/frontend-practice/oh-studio/page.tsx
@@ -11,12 +11,12 @@ function NavBarItem(props: { text: string; href: string }) {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-4">
-      <nav className="fixed top-8 flex flex-row gap-2 rounded-full bg-gray-700 bg-opacity-50 backdrop-blur p-1">
+      <nav className="fixed top-8 flex flex-row gap-2 rounded-full bg-gray-700 bg-opacity-50 p-1 backdrop-blur">
         <NavBarItem href={"/frontend-practice/oh-studio"} text="Home" />
         <NavBarItem href={"/frontend-practice/oh-studio"} text="Profile" />
         <NavBarItem href={"/frontend-practice/oh-studio"} text="Contact" />
       </nav>
-      <h1 className="text-5xl text-center max-w-md">
+      <h1 className="max-w-md text-center text-5xl">
         A web and mobile developer working with clients globally
       </h1>
     </main>
