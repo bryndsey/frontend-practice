2ce1385a5e707f4b95274e933a13bd3a2d1db53a
Bryan <blinz117@gmail.com>
2023-08-05 07:50:51 -0500

    Some minor styling of the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,28 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/frontend-practice/oh-studio">Oh.Studio Practice</Link>
+      <ul>
+        <li className="flex flex-col">
+          <Link
+            href="/frontend-practice/oh-studio"
+            className="text-xl font-bold"
+          >
+            Oh.Studio Practice
+          </Link>
+          <a
+            href="https://www.frontendpractice.com/projects/oh-studio"
+            className="text-sm text-gray-500 hover:text-gray-400"
+          >
+            Frontend Practice Prompt
+          </a>
+          <a
+            href="https://oh.studio/"
+            className="text-sm  text-gray-500 hover:text-gray-400"
+          >
+            Original Website
+          </a>
+        </li>
+      </ul>
     </main>
   );
 }
