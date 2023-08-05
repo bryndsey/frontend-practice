9c72ca74e8663afcb86b9657decdb6aa5ffdc0f4
Bryan <blinz117@gmail.com>
2023-08-05 13:10:12 -0500

    Clean up accessibility issue
    I had a non-li tag child inside of a ul, which was flagged as an issue with accessibility stuff

--- src/app/frontend-practice/oh-studio/page.tsx
+++ src/app/frontend-practice/oh-studio/page.tsx
@@ -10,18 +10,20 @@ export default function Home() {
         <h1 className="max-w-xl break-words text-center text-5xl font-medium leading-tight">
           A web and mobile developer working with clients globally
         </h1>
-        <ul className="hidden flex-row items-center gap-2.5 text-sm md:inline-flex">
+        <div className="hidden flex-row items-center gap-2.5 text-sm md:inline-flex">
           {"Expertise"}
-          <li>
-            <Chip>React</Chip>
-          </li>
-          <li>
-            <Chip>Next.js</Chip>
-          </li>
-          <li>
-            <Chip>Three.js</Chip>
-          </li>
-        </ul>
+          <ul className="flex flex-row items-center gap-2.5">
+            <li>
+              <Chip>React</Chip>
+            </li>
+            <li>
+              <Chip>Next.js</Chip>
+            </li>
+            <li>
+              <Chip>Three.js</Chip>
+            </li>
+          </ul>
+        </div>
       </header>
 
       <section className="grid w-full grid-cols-1 gap-x-4 gap-y-10 p-5 md:grid-cols-2 md:p-10">
