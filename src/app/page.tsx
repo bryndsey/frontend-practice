7ce6fe4fc6af63fa3704633d6d4d6bd05dc02d35
Bryan <blinz117@gmail.com>
2023-08-05 17:01:53 -0500

    Add structure for BASIC/DEPT practice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
+      <ul className="flex flex-col gap-48">
         <li className="flex flex-col">
           <Link
             href="/frontend-practice/oh-studio"
@@ -20,6 +20,27 @@ export default function Home() {
           </a>
           <a
             href="https://oh.studio/"
+            className="text-sm  text-gray-500 hover:text-gray-400"
+          >
+            Original Website
+          </a>
+        </li>
+
+        <li className="flex flex-col">
+          <Link
+            href="/frontend-practice/basic-dept"
+            className="text-xl font-bold"
+          >
+            BASIC/DEPT Practice
+          </Link>
+          <a
+            href="https://www.frontendpractice.com/projects/basic"
+            className="text-sm text-gray-500 hover:text-gray-400"
+          >
+            Frontend Practice Prompt
+          </a>
+          <a
+            href="https://www.basicagency.com/"
             className="text-sm  text-gray-500 hover:text-gray-400"
           >
             Original Website
