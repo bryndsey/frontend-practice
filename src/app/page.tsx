5b0c3dcdd081494df361763270fad0fab96764a4
Bryan <blinz117@gmail.com>
2023-08-06 15:14:19 -0500

    Show "Coming soon" in prod for unfinished work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,34 @@ function ProjectItem({
   href,
   frontendPracticeUrl,
   originalSiteUrl,
+  isInProgress,
 }: {
   name: string;
   href: string;
   frontendPracticeUrl: string;
   originalSiteUrl: string;
+  isInProgress?: boolean;
 }) {
+  const useLink = !isInProgress || process.env.NODE_ENV === "development";
+
   return (
-    <li className="flex flex-col">
-      <Link href={href} className="text-xl font-bold">
-        {name}
-      </Link>
-      <a
-        href={frontendPracticeUrl}
-        className="text-sm text-gray-500 hover:text-gray-400"
-      >
-        Frontend Practice Prompt
-      </a>
-      <a
-        href={originalSiteUrl}
-        className="text-sm  text-gray-500 hover:text-gray-400"
-      >
-        Original Website
-      </a>
+    <li className="flex flex-row justify-between">
+      <div className="flex flex-col">
+        <h2 className="text-xl font-bold">{name}</h2>
+        <a
+          href={frontendPracticeUrl}
+          className="text-sm text-gray-500 hover:text-gray-400"
+        >
+          Frontend Practice Prompt
+        </a>
+        <a
+          href={originalSiteUrl}
+          className="text-sm  text-gray-500 hover:text-gray-400"
+        >
+          Original Website
+        </a>
+      </div>
+      {useLink ? <Link href={href}>VIEW</Link> : <p>Coming soon!</p>}
     </li>
   );
 }
@@ -58,6 +63,7 @@ export default function Home() {
             href="/frontend-practice/basic-dept"
             frontendPracticeUrl="https://www.frontendpractice.com/projects/basic"
             originalSiteUrl="https://www.basicagency.com/"
+            isInProgress
           />
         </ul>
       </section>
