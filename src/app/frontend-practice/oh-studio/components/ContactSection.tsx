a475b7e25fdbc7a4b9d557128455b5bb5735768c
Bryan <blinz117@gmail.com>
2023-08-06 20:24:07 -0500

    Add reveal animations

--- src/app/frontend-practice/oh-studio/components/ContactSection.tsx
+++ src/app/frontend-practice/oh-studio/components/ContactSection.tsx
@@ -1,10 +1,14 @@
+import RevealOnceVisible from "./RevealOnceVisible";
+
 export default function ContactSection() {
   return (
     <section className="my-8 flex flex-col items-center justify-center p-5 text-4xl font-medium leading-tight md:p-10">
-      {"Let's work together."}
-      <a href="#" className="text-neutral-500 hover:text-neutral-400">
-        {"Get in touch."}
-      </a>
+      <RevealOnceVisible>{"Let's work together."}</RevealOnceVisible>
+      <RevealOnceVisible transition={{ delay: 0.075 }}>
+        <a href="#" className="text-neutral-500 hover:text-neutral-400">
+          {"Get in touch."}
+        </a>
+      </RevealOnceVisible>
     </section>
   );
 }
--- src/app/frontend-practice/oh-studio/page.tsx
+++ src/app/frontend-practice/oh-studio/page.tsx
@@ -2,42 +2,54 @@ import Link from "next/link";
 import Chip from "./components/Chip";
 import ContactSection from "./components/ContactSection";
 import { ProjectGridItem } from "./ProjectGridItem";
+import RevealOnceVisible from "./components/RevealOnceVisible";
 
 export default function Home() {
   return (
     <>
       <header className="flex min-h-[379px] flex-col items-center justify-evenly p-5 md:p-10">
-        <h1 className="max-w-xl break-words text-center text-5xl font-medium leading-tight">
-          A web and mobile developer working with clients globally
-        </h1>
-        <div className="hidden flex-row items-center gap-2.5 text-sm md:inline-flex">
-          {"Expertise"}
-          <ul className="flex flex-row items-center gap-2.5">
-            <li>
-              <Chip>React</Chip>
-            </li>
-            <li>
-              <Chip>Next.js</Chip>
-            </li>
-            <li>
-              <Chip>Three.js</Chip>
-            </li>
-          </ul>
-        </div>
+        <RevealOnceVisible>
+          <h1 className="max-w-xl break-words text-center text-5xl font-medium leading-tight">
+            A web and mobile developer working with clients globally
+          </h1>
+        </RevealOnceVisible>
+        <RevealOnceVisible transition={{ delay: 0.25 }}>
+          <div className="hidden flex-row items-center gap-2.5 text-sm md:inline-flex">
+            {"Expertise"}
+            <ul className="flex flex-row items-center gap-2.5">
+              <li>
+                <Chip>React</Chip>
+              </li>
+              <li>
+                <Chip>Next.js</Chip>
+              </li>
+              <li>
+                <Chip>Three.js</Chip>
+              </li>
+            </ul>
+          </div>
+        </RevealOnceVisible>
       </header>
 
-      <section className="grid w-full grid-cols-1 gap-x-4 gap-y-10 p-5 md:grid-cols-2 md:p-10">
-        <ProjectGridItem text="Portfolio" />
-        <ProjectGridItem text="Creative Developer" />
-        <ProjectGridItem text="SongSpark" />
-      </section>
+      <RevealOnceVisible transition={{ delay: 0.5 }} className="w-full">
+        <section className="grid w-full grid-cols-1 gap-x-4 gap-y-10 p-5 md:grid-cols-2 md:p-10">
+          <ProjectGridItem text="Portfolio" />
+          <ProjectGridItem text="Creative Developer" />
+          <ProjectGridItem text="SongSpark" />
+        </section>
+      </RevealOnceVisible>
 
-      <Link
-        href="/"
-        className="fixed bottom-10 mx-auto rounded-full bg-black px-6 py-3 text-sm text-white"
+      <RevealOnceVisible
+        transition={{ delay: 0.75 }}
+        className="fixed bottom-10"
       >
-        Back
-      </Link>
+        <Link
+          href="/"
+          className="mx-auto rounded-full bg-black px-6 py-3 text-sm text-white"
+        >
+          Back
+        </Link>
+      </RevealOnceVisible>
 
       <ContactSection />
     </>
