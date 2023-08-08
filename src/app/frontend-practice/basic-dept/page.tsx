64a3e69b20ca20603ea938be5f815a1def446796
Bryan <blinz117@gmail.com>
2023-08-08 05:52:44 -0500

    Move engagements to new file

--- src/app/frontend-practice/basic-dept/page.tsx
+++ src/app/frontend-practice/basic-dept/page.tsx
@@ -1,19 +1,5 @@
+import { FeaturedEngagments } from "./FeaturedEngagments";
 import { LinkButton } from "./components/LinkButton";
-
-function EngagementItem() {
-  return (
-    <li className="w-[75vw] flex-shrink-0 sm:w-[40vw] md:w-[30vw]">
-      <div className="h-10 w-10 rounded-full bg-stone-800"></div>
-      <h3 className="relative mt-8 pt-12 font-bold before:absolute before:top-0 before:block before:h-[2px] before:w-5 before:bg-slate-800">
-        CLIENT NAME
-      </h3>
-      <p className="mt-5 text-sm">
-        This is some text about this client. It explains who they are and what I
-        did for them.
-      </p>
-    </li>
-  );
-}
 
 function NewsPreviewItem() {
   return (
@@ -83,23 +69,6 @@ export default function Page() {
 
       <Footer />
     </div>
-  );
-}
-
-function FeaturedEngagments() {
-  return (
-    <section className="overflow-visible p-11">
-      <h2>FEATURED ENGAGMENTS</h2>
-
-      <ul className="relative -inset-x-11 flex w-screen flex-row gap-4 overflow-x-auto px-11 pt-16">
-        <EngagementItem />
-        <EngagementItem />
-        <EngagementItem />
-        <EngagementItem />
-        <EngagementItem />
-        <EngagementItem />
-      </ul>
-    </section>
   );
 }
 
