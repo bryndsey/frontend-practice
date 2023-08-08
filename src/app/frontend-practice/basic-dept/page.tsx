bb1b18e84d1aa040858943c9bdd672ff6f3d6aaa
Bryan <blinz117@gmail.com>
2023-08-07 21:39:35 -0500

    Add better news items

--- src/app/frontend-practice/basic-dept/page.tsx
+++ src/app/frontend-practice/basic-dept/page.tsx
@@ -27,6 +27,28 @@ function EngagementItem() {
   );
 }
 
+function NewsPreviewItem() {
+  return (
+    <a
+      href="#"
+      className="relative flex w-full flex-col gap-2 pt-5 before:absolute before:left-0 before:right-0 before:top-0 before:h-[1px] before:bg-stone-800 sm:flex-row"
+    >
+      <div className="aspect-[4/3] w-full bg-orange-800 sm:w-1/3"></div>
+      <div className="relative flex flex-1 flex-col justify-between gap-8">
+        <h3 className="max-w-[75%] text-2xl">
+          THIS IS THE TITLE OF THE ARTICLE. PLEASE READ ME.
+        </h3>
+        <figure className="absolute bottom-0 right-0 box-content h-7 w-7 text-2xl sm:top-0">
+          {"->"}
+        </figure>
+        <span className="text-xs">
+          <b>PRESS</b> 8.7.23
+        </span>
+      </div>
+    </a>
+  );
+}
+
 export default function Page() {
   return (
     <div className="bg-neutral-100 text-stone-800">
@@ -85,12 +107,25 @@ export default function Page() {
             <LinkButton>VIEW ALL</LinkButton>
           </div>
 
-          <ul>
-            <li>PLACEHOLDER</li>
-            <li>PLACEHOLDER</li>
-            <li>PLACEHOLDER</li>
-            <li>PLACEHOLDER</li>
-            <li>PLACEHOLDER</li>
+          <ul className="mt-8 flex flex-col gap-5 sm:gap-10">
+            <li>
+              <NewsPreviewItem />
+            </li>
+            <li>
+              <NewsPreviewItem />
+            </li>
+            <li>
+              <NewsPreviewItem />
+            </li>
+            <li>
+              <NewsPreviewItem />
+            </li>
+            <li>
+              <NewsPreviewItem />
+            </li>
+            <li>
+              <NewsPreviewItem />
+            </li>
           </ul>
         </section>
       </main>
