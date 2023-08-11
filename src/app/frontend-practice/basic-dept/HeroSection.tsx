e5a37b52e830e0811b873e93ed1103220abc3926
Bryan <blinz117@gmail.com>
2023-08-11 16:03:07 -0500

    Animate cursor position

--- src/app/frontend-practice/basic-dept/HeroSection.tsx
+++ src/app/frontend-practice/basic-dept/HeroSection.tsx
@@ -1,32 +1,54 @@
 "use client";
 
+import { motion, useSpring } from "framer-motion";
 import { useRef } from "react";
-
-const cursorSizeInPx = 96;
 
 export function HeroSection() {
   const customCursorContainerRef = useRef<HTMLDivElement>(null!);
   const customCursorRef = useRef<HTMLDivElement>(null!);
 
+  const cursorSpringX = useSpring(0, {
+    damping: 15,
+    mass: 0.15,
+    stiffness: 200,
+  });
+  const cursorSpringY = useSpring(0, {
+    damping: 15,
+    mass: 0.15,
+    stiffness: 200,
+  });
+
   return (
     <section
       className="relative h-screen w-full bg-green-500"
       onPointerMove={(e) => {
-        const position = { x: e.clientX, y: e.clientY };
-        customCursorContainerRef.current.style.top = `${
-          position.y - customCursorRef.current.clientHeight / 2
-        }px`;
-        customCursorContainerRef.current.style.left = `${
-          position.x - customCursorRef.current.clientWidth / 2
-        }px`;
+        const position = { x: e.pageX, y: e.pageY };
+        cursorSpringX.set(position.x - customCursorRef.current.clientWidth / 2);
+        cursorSpringY.set(
+          position.y - customCursorRef.current.clientHeight / 2,
+        );
+      }}
+      onPointerLeave={(e) => {
+        cursorSpringX.set(
+          e.currentTarget.clientWidth / 2 -
+            customCursorRef.current.clientWidth / 2,
+        );
+        cursorSpringY.set(
+          e.currentTarget.clientHeight / 2 -
+            customCursorRef.current.clientHeight / 2,
+        );
       }}
     >
-      <div className="absolute" ref={customCursorContainerRef}>
+      <motion.div
+        ref={customCursorContainerRef}
+        style={{ x: cursorSpringX, y: cursorSpringY }}
+        className="pointer-events-none absolute"
+      >
         <div
           className={`h-24 w-24 rounded-full bg-neutral-100`}
           ref={customCursorRef}
         ></div>
-      </div>
+      </motion.div>
     </section>
   );
 }
