be429d0a2c99b0d10e14219c8a758cd90dc7f670
Bryan <blinz117@gmail.com>
2023-08-12 10:36:53 -0500

    Update link button style
    Including hover animation

--- src/app/frontend-practice/basic-dept/components/LinkButton.tsx
+++ src/app/frontend-practice/basic-dept/components/LinkButton.tsx
@@ -6,7 +6,10 @@ export function LinkButton({
   children: React.ReactNode;
 }) {
   return (
-    <a href={link ?? "#"} className="rounded-full border px-4 py-2 text-xs">
+    <a
+      href={link ?? "#"}
+      className="relative z-10 overflow-clip rounded-full border border-slate-800 px-7 pb-1.5 pt-2 text-xs font-semibold transition-all duration-[250ms] before:absolute before:inset-0 before:-z-10 before:translate-y-full before:bg-stone-800 before:transition-all before:duration-[250ms] hover:text-neutral-100 hover:before:translate-y-0"
+    >
       {children}
     </a>
   );
