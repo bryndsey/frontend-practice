740d0d215dd3dad84c7181323eae016c30ebfd49
Bryan <blinz117@gmail.com>
2023-08-13 14:47:51 -0500

    Change background color based on scroll position
    Basically, detect when the relevant view is a certain amount in the view and perform the transition then

--- src/app/frontend-practice/basic-dept/SpotlightSection.tsx
+++ src/app/frontend-practice/basic-dept/SpotlightSection.tsx
@@ -1,8 +1,30 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import { LinkButton } from "./components/LinkButton";
+import { useInView } from "@react-spring/web";
 
 export function SpotlightSection() {
+  const [sectionRef, inView] = useInView({
+    amount: 0.25,
+  });
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (inView) {
+      root.style.setProperty("--color-content", "249 205 205");
+      root.style.setProperty("--color-backdrop", "37 36 34");
+    } else {
+      root.style.setProperty("--color-content", "37 36 34");
+      root.style.setProperty("--color-backdrop", "244 244 244");
+    }
+  }, [inView]);
+
   return (
-    <section className="flex flex-col-reverse items-start gap-2 p-10 md:grid md:grid-cols-2 xl:gap-16 xl:p-20">
+    <section
+      className="flex flex-col-reverse items-start gap-2 p-10 md:grid md:grid-cols-2 xl:gap-16 xl:p-20"
+      ref={sectionRef}
+    >
       <div className="sticky top-28 flex flex-col items-start gap-6">
         <q className="text-5xl font-bold xl:text-7xl">
           BRYAN/LINZ® HELPS BRANDS ● CONNECT W/ CULTURE
