43d16c1e4d14d8e57f6b6e9c90e568881e6baee6
Bryan <blinz117@gmail.com>
2023-08-16 14:52:02 -0500

    Try initial API endpoint
    Especially trying to test to see if I can provide images from my local project files like this

---
+++ src/app/frontend-practice/basic-dept/api/news/route.ts
@@ -0,0 +1,33 @@
+import PlaceholderImage from "@assets/pexels-emilio-gonzález-17781404.jpg";
+
+export async function GET(request: Request) {
+  return new Response(
+    JSON.stringify([
+      {
+        title: "First News Article",
+        subtitle: "This is content about what the first article contains",
+        dateString: "8.15.23",
+        thumbnailUrl: PlaceholderImage.src,
+      },
+      {
+        title: "Second News Article",
+        subtitle: "This is content about what the second article contains",
+        dateString: "8.14.23",
+        thumbnailUrl: PlaceholderImage.src,
+      },
+      {
+        title: "A Third News Article",
+        subtitle: "This is content about what the third article contains",
+        dateString: "8.13.23",
+        thumbnailUrl: PlaceholderImage.src,
+      },
+      {
+        title: "One More News Article",
+        subtitle: "This is content about what the last article contains",
+        dateString: "8.12.23",
+        thumbnailUrl: PlaceholderImage.src,
+      },
+    ]),
+    { status: 200 },
+  );
+}
