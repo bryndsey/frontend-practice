6a7d875d0b0285db8a26f97ddaf7038a3a4e62cd
Bryan <blinz117@gmail.com>
2023-08-17 04:54:47 -0500

    Attempt to better handle build step

--- src/app/frontend-practice/basic-dept/api/config.ts
+++ src/app/frontend-practice/basic-dept/api/config.ts
@@ -1,9 +1,13 @@
+const isBuild =
+  process.env.NEXT_PHASE !== undefined &&
+  process.env.NEXT_PHASE.includes("build");
+
 const projectPath = "/frontend-practice/basic-dept/api";
 
 const devUrl = `http://localhost:${process.env.PORT}${projectPath}`;
 
 const productionUrl =
-  process.env.VERCEL_URL === undefined
+  process.env.VERCEL_URL === undefined || isBuild
     ? undefined
     : `https://${process.env.VERCEL_URL}${projectPath}`;
 
