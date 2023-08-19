7672340f5a9265bebe74fb7ae39e83e0c758a237
Bryan <blinz117@gmail.com>
2023-08-19 11:44:41 -0500

    Clean up unneeded code

--- src/app/frontend-practice/basic-dept/api/news/fetchNews.ts
+++ src/app/frontend-practice/basic-dept/api/news/fetchNews.ts
@@ -1,14 +1,9 @@
 import { urlRoot } from "../config";
 
 export default async function fetchNews() {
-  if (urlRoot === undefined) {
-    return null;
-  }
-
   try {
     const res = await fetch(`${urlRoot}/news`);
 
-    console.log(res);
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
