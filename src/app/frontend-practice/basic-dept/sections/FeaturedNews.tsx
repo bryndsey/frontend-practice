ab323427b087e2a59c40b9b826e1a212db7ec609
Bryan <blinz117@gmail.com>
2023-08-19 11:25:23 -0500

    Remove unneeded hardcoded items

--- src/app/frontend-practice/basic-dept/sections/FeaturedNews.tsx
+++ src/app/frontend-practice/basic-dept/sections/FeaturedNews.tsx
@@ -22,24 +22,6 @@ export async function FeaturedNews() {
       </div>
 
       <ul className="mt-8 flex flex-col gap-5 md:gap-10">
-        <li>
-          <NewsPreviewItem />
-        </li>
-        <li>
-          <NewsPreviewItem />
-        </li>
-        <li>
-          <NewsPreviewItem />
-        </li>
-        <li>
-          <NewsPreviewItem />
-        </li>
-        <li>
-          <NewsPreviewItem />
-        </li>
-        <li>
-          <NewsPreviewItem />
-        </li>
         {(data as NewsArticle[]).map((article) => (
           <NewsItem key={article.title} {...article} />
         ))}
@@ -78,28 +60,6 @@ function NewsItem({
         <span className="text-xs">
           {<b>PRESS </b>}
           {dateString}
-        </span>
-      </div>
-    </a>
-  );
-}
-
-function NewsPreviewItem() {
-  return (
-    <a
-      href="#"
-      className="before:bg-content group relative flex w-full flex-col gap-4 pt-5 before:absolute before:left-0 before:right-0 before:top-0 before:h-[1px] md:flex-row"
-    >
-      <div className="aspect-[4/3] w-full bg-orange-800 md:w-1/3"></div>
-      <div className="relative flex flex-1 flex-col justify-between gap-8">
-        <h3 className="max-w-[75%] text-2xl group-hover:underline">
-          THIS IS THE TITLE OF THE ARTICLE. PLEASE READ ME.
-        </h3>
-        <figure className="absolute bottom-0 right-0 box-content text-2xl md:top-0 md:text-5xl">
-          {"->"}
-        </figure>
-        <span className="text-xs">
-          <b>PRESS</b> 8.7.23
         </span>
       </div>
     </a>
