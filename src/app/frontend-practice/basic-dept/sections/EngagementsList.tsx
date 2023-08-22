cb8c7467a24a9a8f17c3d1bca20d83710b4cc826
Bryan <blinz117@gmail.com>
2023-08-22 09:50:05 -0500

    Extract/reuse item component

--- src/app/frontend-practice/basic-dept/sections/EngagementsList.tsx
+++ src/app/frontend-practice/basic-dept/sections/EngagementsList.tsx
@@ -12,35 +12,21 @@ export async function EngagementsList() {
   return (
     <ul className="relative flex flex-row gap-4">
       {(data as Engagement[]).map((engagement) => (
-        <li
-          className="w-[75vw] flex-shrink-0 md:w-[40vw] lg:w-[30vw]"
-          key={engagement.clientName}
-        >
-          <figure className="font-serif text-4xl font-extrabold italic">
-            {engagement.logoString}
-          </figure>
-          <h3 className="before:bg-content relative mt-8 pt-12 font-bold uppercase before:absolute before:top-0 before:block before:h-[2px] before:w-5">
-            {engagement.clientName}
-          </h3>
-          <p className="mt-5 text-sm">{engagement.about}</p>
-        </li>
+        <EngagementItem key={engagement.clientName} {...engagement} />
       ))}
     </ul>
   );
 }
-function EngagementItem() {
+function EngagementItem({ logoString, clientName, about }: Engagement) {
   return (
     <li className="w-[75vw] flex-shrink-0 md:w-[40vw] lg:w-[30vw]">
       <figure className="font-serif text-4xl font-extrabold italic">
-        LOGO
+        {logoString}
       </figure>
-      <h3 className="before:bg-content relative mt-8 pt-12 font-bold before:absolute before:top-0 before:block before:h-[2px] before:w-5">
-        CLIENT NAME
+      <h3 className="before:bg-content relative mt-8 pt-12 font-bold uppercase before:absolute before:top-0 before:block before:h-[2px] before:w-5">
+        {clientName}
       </h3>
-      <p className="mt-5 text-sm">
-        This is some text about this client. It explains who they are and what I
-        did for them.
-      </p>
+      <p className="mt-5 text-sm">{about}</p>
     </li>
   );
 }
