6d01ac1ee13b44b9de24c50c9f6c2a311cd2a219
Bryan <blinz117@gmail.com>
2023-08-27 11:05:50 -0500

    Fix custom font for Oh-studio page
    I need the custom font classes to be contained within the oh-studio-root element, so I wrapped the layout with another layer

--- src/app/frontend-practice/oh-studio/layout.tsx
+++ src/app/frontend-practice/oh-studio/layout.tsx
@@ -26,39 +26,40 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main
-      id="oh-studio-root"
-      className={`flex min-h-screen flex-col items-center ${font.variable} font-sans`}
-    >
-      <NavBar />
+    <div id="oh-studio-root">
+      <main
+        className={`flex min-h-screen flex-col items-center ${font.variable} font-sans`}
+      >
+        <NavBar />
 
-      <div className="mt-24 grid w-full flex-grow place-items-center">
-        {children}
-      </div>
+        <div className="mt-24 grid w-full flex-grow place-items-center">
+          {children}
+        </div>
 
-      <footer className="flex w-full flex-col justify-between p-5 pb-20 text-sm md:flex-row md:p-10 md:pb-10">
-        <div className="flex flex-col items-center gap-2 md:flex-row">
-          <div className="aspect-square w-6 rounded-full bg-black"></div>
-          <a href="https://oh.studio/">{"Design by Oli Harris"}</a>
-        </div>
-        <div className="flex flex-row items-center justify-center gap-4">
-          <a
-            href="https://github.com/bryndsey"
-            className="hover:text-neutral-500"
-          >
-            Github
-          </a>
-          <a
-            href="https://www.linkedin.com/in/bryan-c-lindsey/"
-            className="hover:text-neutral-500"
-          >
-            LinkedIn
-          </a>
-          <a href="#" className="hover:text-neutral-500">
-            Mail
-          </a>
-        </div>
-      </footer>
-    </main>
+        <footer className="flex w-full flex-col justify-between p-5 pb-20 text-sm md:flex-row md:p-10 md:pb-10">
+          <div className="flex flex-col items-center gap-2 md:flex-row">
+            <div className="aspect-square w-6 rounded-full bg-black"></div>
+            <a href="https://oh.studio/">{"Design by Oli Harris"}</a>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-4">
+            <a
+              href="https://github.com/bryndsey"
+              className="hover:text-neutral-500"
+            >
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bryan-c-lindsey/"
+              className="hover:text-neutral-500"
+            >
+              LinkedIn
+            </a>
+            <a href="#" className="hover:text-neutral-500">
+              Mail
+            </a>
+          </div>
+        </footer>
+      </main>
+    </div>
   );
 }
