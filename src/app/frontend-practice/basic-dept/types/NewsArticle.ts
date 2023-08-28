d044562732ef9ac8ab21872514313507476ebe90
Bryan <blinz117@gmail.com>
2023-08-27 20:40:59 -0500

    Validate news article input

--- src/app/frontend-practice/basic-dept/types/NewsArticle.ts
+++ src/app/frontend-practice/basic-dept/types/NewsArticle.ts
@@ -1,8 +1,18 @@
-export type NewsArticle = {
-  title: string;
-  subtitle: string;
-  dateString: string;
-  thumbnailUrl: string;
-  thumbnailWidth: number;
-  thumbnailHeight: number;
-};
+import { z } from "zod";
+
+const ZNewsArticle = z.object({
+  title: z.string(),
+  subtitle: z.string(),
+  dateString: z.string(),
+  thumbnailUrl: z.string(),
+  thumbnailWidth: z.number().int().positive(),
+  thumbnailHeight: z.number().int().positive(),
+});
+
+export type NewsArticle = z.infer<typeof ZNewsArticle>;
+
+const ZNewsArticleList = z.array(ZNewsArticle);
+
+export function parseNewsArticleList(input: unknown): NewsArticle[] {
+  return ZNewsArticleList.parse(input);
+}
